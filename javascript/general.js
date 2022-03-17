6cef32c3708dec80d5424732f5811da0dab2a398
hieutri <trihivu@gmail.com>
2022-03-17 17:19:35 +0700

    fuck linux

--- javascript/general.js
+++ javascript/general.js
@@ -17,10 +17,13 @@ function openLogin() {
     loginForm.style.display = "flex"
 }
 
+//login -- register form start
 const login = document.getElementById("login");
 const register = document.getElementById("register");
 const loginForm = document.getElementById("login-form");
 const registerForm = document.getElementById("register-form");
+const loginSumbit = document.getElementById("login-sumbit");
+const registerSumbit = document.getElementById("register-sumbit")
 login.addEventListener('click', () => {
     loginForm.style.display = "flex"
     registerForm.style.display = "none"
@@ -28,6 +31,41 @@ login.addEventListener('click', () => {
 register.addEventListener('click', () => {
     loginForm.style.display = "none"
     registerForm.style.display = "flex"
+
+    //register form
+    let users = [];
+
+    // Nen co dong nay.
+    if (localStorage.getItem("users") == null) {
+        localStorage.setItem("users", JSON.stringify(users))
+    }
+    // LocalStorage: luu tru duoi dang String (chuoi)
+    // if(localStorage.getItem("users"));
+     if (userEmail.length == 0) alert("Please fill in all the required fields!");
+    else if (userPassword.length == 0) alert("Please fill in all the required fields!");
+    else {
+        registerSumbit.addEventListener("submit", (event) => {
+            event.preventDefault();
+            // dung de chan su kien reload mac dinh cua trinh duyet
+            const userEmail = document.getElementById("register-email").value;
+            const userPassword = document.getElementById("register-password").value;
+            const data = {
+                email: registerSumbit.email,
+                password: registerSumbit.password
+            }
+            console.log(data);
+            // LAY DL tren localStorage ve
+            let getUsersOnLocalStorage = JSON.parse(localStorage.getItem("users"));
+            getUsersOnLocalStorage.push(data);
+            localStorage.setItem('users', JSON.stringify(getUsersOnLocalStorage))
+    
+            registerSumbit.email.value = ""
+            registerSumbit.password.value = ""
+    
+            // windows.location.href = '../signIn.html'
+            window.location = loginForm;
+        })
+    }
 })
 window.onclick = (event) => {
     if (event.target == headerRes) {
@@ -41,6 +79,43 @@ window.onclick = (event) => {
     }
 
 }
+loginSumbit.addEventListener('click', event => {
+    console.log("test");
+    event.preventDefault();
+
+    const userEmail = document.getElementById("login-email").value;
+    const userPassword = document.getElementById("login-password").value;
+
+    if (userEmail.length == 0) alert("Please fill in all the required fields!");
+    else if (userPassword.length == 0) alert("Please fill in all the required fields!");
+    else {
+        const userLogin = {
+            email: userEmail,
+            password: userPassword
+        }
+        console.log(userLogin);
+
+        let usersFromLocalStorage = JSON.parse(localStorage.getItem("users"));
+        console.log("usersFromLocalStorage: ", usersFromLocalStorage);
+
+        // kiem tra thong tin dang nhap voi du lieu dang ky
+        console.log(usersFromLocalStorage.find(item => item.email == userLogin.email));
+        if (usersFromLocalStorage.find(item => item.email == userLogin.email) != undefined) {
+            let value = usersFromLocalStorage.find(item => item.email == userLogin.email);
+            if (value.password == userLogin.password) {
+                alert('login success')
+            } else {
+                alert("Wrong password")
+            }
+        }
+        signIn.email.value = ""
+        signIn.password.value = ""
+    }
+})
+//login -- register form end
+
+
+
 fetch("/json/product-data.json")
     .then((respone) => respone.json())
     .then((data) => {
@@ -85,13 +160,13 @@ fetch("/json/product-data.json")
         let addProductBtn = document.querySelectorAll(".add-to-cart");
         const cartInfo = document.querySelector(".cart-info")
         var counter = 1;
-        if(counter == 1) cartInfo.innerHTML = "Your Cart Is Empty";
+        if (counter == 1) cartInfo.innerHTML = "Your Cart Is Empty";
         else cartInfo.innerHTML = " "
         for (let i = 0; i < data.products.length; i++) {
             let productCounter = 0;
             let cartProductImg = data.products[i].img;
-            let cartProductName =  data.products[i].name;
-            let cartProductSwitch =  data.products[i].switch;
+            let cartProductName = data.products[i].name;
+            let cartProductSwitch = data.products[i].switch;
             let cartProductPrice = data.products[i].price
             let productInfo = {
                 productImg: `${cartProductImg}`,
@@ -114,8 +189,8 @@ fetch("/json/product-data.json")
                 if (productCounter > 1) {
                     alert("This Product Has Already Been Added To Your Cart!")
                 }
-                else{
-                    counter ++;
+                else {
+                    counter++;
                     cartProductContainer.innerHTML += `
                 <div class="cart-product">
                 <div class="cart-product-img"><img src="${product.productImg}"></div>
@@ -124,13 +199,13 @@ fetch("/json/product-data.json")
                 <button class = "cart-remove-btn">Remove</button>
                 </div>
                 `;
-                alert("Succesfully Added A Product")
+                    alert("Succesfully Added A Product")
                 }
                 let removeProductBtn = document.getElementsByClassName("cart-remove-btn")
                 for (let i = 0; i < removeProductBtn.length; i++) {
                     let btn = removeProductBtn[i]
                     btn.addEventListener("click", (event) => {
-                        counter --;
+                        counter--;
                         console.log(counter);
                         productCounter--;
                         console.log(productCounter);
